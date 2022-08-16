f63002f9dd1a6970037426db896dce709ee72670
Scott Bennett <scottbennett912@gmail.com>
2022-08-15 22:24:30 -0500

    Implement some hacks to appease the linker

---
+++ lib/cxx/src/cxx_hacks.cpp
@@ -0,0 +1,18 @@
+extern "C" int __cxa_atexit(void (*func)(void*), void* arg, void* dso_handle) {
+    // This is normally called in order to run the destructors of global objects
+    // Since that will never actually happen in (our) wasm, we can safely ignore these calls
+    return 0;
+}
+
+// These are what the clang+linker wasm backend generates in order to actually run the global object ctors/dtors
+// Since we won't be using those either, we can create a main() that calls them to trick the linker into thinking that
+// we used them. And then by not exporting main(), main() will be removed tricking it into also removing the
+// __wasm_call_ctors() and __wasm_call_dtors()
+extern "C" void __wasm_call_ctors();
+// extern "C" void __wasm_call_dtors();
+
+int main() {
+    __wasm_call_ctors();
+    // __wasm_call_dtors();
+    return 0;
+}
